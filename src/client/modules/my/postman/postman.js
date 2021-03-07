70cf885cef8b2ce477e70572c307642b270ad664
aritram1 <aritram1@gmail.com>
2021-03-08 00:50:45 +0530

    changes to css

--- src/client/modules/my/postman/postman.js
+++ src/client/modules/my/postman/postman.js
@@ -1,17 +1,21 @@
 import { LightningElement, track, api } from 'lwc';
+const DEFAULT_GRADIENT_CHANGE = 0.09;
+const DEFAULT_STYLE = "background-color: rgba(5, 122, 218, ";
+const DEFAULT_ERROR_STYLE = "background-color: rgba(255, 0, 0, ";
 
 
 export default class Postman extends LightningElement {
     endpoint = 'https://dog.ceo/api/breeds/image/random';//'http://0.0.0.0:3002'; //''https://jsonplaceholder.typicode.com/posts/';
     @track history=[];
-    @track response = 'Default response';
+    @track response = 'Response will appear here';
     @track timetaken = '00';
     @track method = 'GET';
 
+    itemstyle = "background-color: rgba(5, 122, 218, 0)";
+    alpha = 0;
+
     handleClick(event){
         console.log(`Button pressed`);
-        //console.log(`Node retrieved : ${document.querySelector('.top--ibar')}`);
-        //this.endpoint = document.querySelector('.top--ibar').value;
         console.log(`Endpoint is : ${this.endpoint}`);
         let start_time = Date.now();
         fetch(this.endpoint, {
@@ -26,14 +30,19 @@ export default class Postman extends LightningElement {
         })
         .then(data=>{
             console.log(data);
-            this.response = JSON.stringify(data);
+            //this.response = JSON.stringify(data);
+            this.response = this.process(data);
+            
+            this.timetaken = Date.now() - start_time;
+            
             this.history.unshift({
                 id: Math.random(1),
                 ep: this.endpoint,
                 value: this.endpoint.length < 30 ? this.endpoint : this.endpoint.substr(0,30) + '....',
-                success: true
+                success: true,
+                style : this.getStyle(this.itemstyle,true)
             });
-            this.timetaken = Date.now() - start_time;
+
         })
         .catch(error=>{
             console.log(`Error happened inside resp block ${error}`);
@@ -42,12 +51,26 @@ export default class Postman extends LightningElement {
                 id: Math.random(1),
                 ep: this.endpoint,
                 value: this.endpoint.length < 30 ? this.endpoint : this.endpoint.substr(0,30) + '....',
-                success: false
+                success: false,
+                style : this.getStyle(this.itemstyle, false)
             });
             this.timetaken = Date.now() - start_time;
         });
     }
     
+    getStyle(currentstyle, success){
+        console.log('as received : ' + this.itemstyle);
+        let start = currentstyle.lastIndexOf(',') + 1;
+        let end = currentstyle.indexOf(')') - 2;
+        let alpha = parseFloat(currentstyle.substr(start, end));
+        console.log('alpha->'+alpha);
+        this.alpha = (alpha + DEFAULT_GRADIENT_CHANGE).toFixed(2);
+        if(this.alpha > 1) this.alpha = 1;
+        this.itemstyle = success ? DEFAULT_STYLE + this.alpha + ")" : DEFAULT_ERROR_STYLE + this.alpha + ")";
+        console.log('as sent back : ' + this.itemstyle);
+        return this.itemstyle;
+    }
+
     handleChange(event){
         console.log(`Value input as : ${event.target.value}`);
         this.endpoint = event.target.value;
@@ -62,5 +85,16 @@ export default class Postman extends LightningElement {
         this.endpoint = event.target.getAttributeNode("name").value;//event.target.textContent;
         console.log('inside handleLineItemClick:' + this.endpoint);
         this.handleClick(event);
+    }
+
+    process(data){
+        console.log(JSON.stringify(data, undefined, 4));
+        return JSON.stringify(data, undefined, 4);
+        // let r = "";
+        // for(let d in data){
+        //     r += "\n\n\n\n\n"+ d+ ":"+data[d];
+        // }
+        // console.log(r)
+        // return r;
     }
 }
