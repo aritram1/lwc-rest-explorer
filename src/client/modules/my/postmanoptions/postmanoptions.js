34de0fbc0e1ee8a53bf72ccc35a9e6f958623174
aritram1 <aritram1@gmail.com>
2021-03-17 04:01:14 +0530

    Header tab is kept off now

--- src/client/modules/my/postmanoptions/postmanoptions.js
+++ src/client/modules/my/postmanoptions/postmanoptions.js
@@ -173,10 +173,10 @@ export default class Postmanoptions extends LightningElement {
     // }
     
     handleTabClick(e){
-        // this.showAuth = false;
-        // this.showHeaders = false;
-        // this.showBody = false;
-        // this.showRaw = false;
+        this.showAuth = false;
+        this.showHeaders = false;
+        this.showBody = false;
+        this.showRaw = false;
 
         let tabName = e.target.getAttribute('name');
         console.log('Inside handleTabClick. Tab clicked - ' + tabName);
@@ -188,9 +188,9 @@ export default class Postmanoptions extends LightningElement {
                 if(this.method === 'GET' && tabName === 'Body'){
                     alert('Body is not applicable for GET requests');
                 }
-                else if(tabName === 'Headers'){
-                    alert('Send data as params instead of headers for now!');
-                }
+                // else if(tabName === 'Headers'){
+                //     alert('Send data as params instead of headers for now!');
+                // }
                 else{
                     tab.classList.add('active');
                 }
